2d5a57958f7d5a02d40edaf8c05ab800406544a2
rajibhossainpavel <rajibh.pavel@gmail.com>
2022-05-29 01:30:13 +0600

    backend!

---
+++ url-shorten-frontend/composables/url-shorten.ts
@@ -0,0 +1,27 @@
+export function useHelloWorld() {
+	async function storeUrl(payload){
+		const requestOptions = {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify(payload)
+		};
+		let result={};
+		try{
+			const url="http://localhost:8000/store-url";
+			const response= await fetch(url, requestOptions).then(async (res)=>{
+				const responseData= await res.json();
+				if(responseData.status==200){
+					result=responseData.data.original.data;
+					return result;
+				}
+			});
+		}
+		catch(e){
+			return {'status': 400, 'data': result};
+		}
+		return {'status': 200, 'data': result};
+	};
+	return {
+		getIP
+	};
+}	
