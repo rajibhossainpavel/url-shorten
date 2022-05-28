2067a0a1e8f552ef40540f0535d486a126d90cf6
rajibhossainpavel <rajibh.pavel@gmail.com>
2022-05-29 01:19:22 +0600

    frontend!

--- url-shorten-frontend/app.vue
+++ url-shorten-frontend/app.vue
@@ -1,5 +1,13 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+	<div class="grid place-items-center h-screen">
+		<div class="flex">
+			<span class="text-sm border border-2 rounded-l px-4 py-2 bg-gray-300 whitespace-no-wrap">URL</span>
+			<input name="field_name" class="border border-2 rounded-r px-4 py-2 w-full" type="text" placeholder="Type URL" />
+			<button class="btn">Get Short URL</button>
+		</div>
+		<div class="text-center">
+		<p class="py-6">pfzgdjkghjklsdfhgsdfjkhkjsdfjksd;fkh;ksdfjdfj</p>
+	</div>
+	</div>
+	
 </template>
